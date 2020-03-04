c1cd328c09435d81e63f4cd7b646e83a2d94aa17
iryna_naz <iryna@gmail.com>
2020-03-04 14:48:56 +0100

    schema altered

--- populate.sh
+++ populate.sh
@@ -5,14 +5,17 @@
                             --delimiter ";"  \
                             --array-delimiter "|"  \
                             --id-type=INTEGER  \
-                            --nodes=Author="path"  \
-                            --nodes=Journal=""  \
-                            --nodes=Article=""  \
+                            --nodes=Author="output_author.csv"  \
+                            --nodes=Article="/Users/irinanazarchuk/Documents/uni/SDM/output_/output_article_header.csv, /Users/irinanazarchuk/Documents/uni/SDM/output_/output_article.csv"  \
                             --nodes=Conference-Workshop="path" \
                             --nodes=Keyword="path" \
+                            --nodes=Topic="path" \
+                            --nodes=Editor="output_editor.csv" \
                             --relationships=Cites="path" \
                             --relationships=Presented_In="path" \
-                            --relationships=Writes="path" \
+                            --relationships=Writes="output_author_authored_by.csv" \
                             --relationships=Reviews="path" \
                             --relationships=Has="path"  \
                             --relationships=Published_In=""  \
+                            --relationships=Relates_To=""  \
+                            --relationships=Edited_By="output_editor_edited_by.csv"  \
