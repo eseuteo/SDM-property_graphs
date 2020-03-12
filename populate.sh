9804e376ecd76ffb60e4082944d9b2363d38d04d
eseuteo <ricardoholthausen@gmail.com>
2020-03-12 18:17:26 +0100

    changed populating script according to neo4j 3.x version

--- populate.sh
+++ populate.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-./bin/neo4j-admin import    --skip-bad-relationships=true  \
+./bin/neo4j-admin import    --ignore-missing-nodes=true  \
                             --database=sdm-lab1  \
                             --delimiter ";"  \
                             --array-delimiter "|" \
                             --id-type=INTEGER  \
-                            --nodes=Article="<path-to-folder>/article_node.csv"  \
-                            --nodes=Author="<path-to-folder>/author_node.csv"  \
-                            --nodes=Conference/Workshop="<path-to-folder>/conference_workshop_node.csv" \
-                            --nodes=Journal="<path-to-folder>/journal_node.csv"  \
-                            --nodes=Keyword="<path-to-folder>/keyword_node.csv" \
-                            --relationships=Cites="<path-to-folder>/cites_relationship.csv" \
-                            --relationships=Has_Keyword="<path-to-folder>/has_keyword_relationship.csv"  \
-                            --relationships=Presented_In="<path-to-folder>/presented_in_relationship.csv" \
-                            --relationships=Published_In="<path-to-folder>/published_in_relationship.csv"  \
-                            --relationships=Reviews="<path-to-folder>/reviews_relationship.csv" \
-                            --relationships=Writes="<path-to-folder>/writes_relationship.csv" \
+                            --nodes:Article="$data_path/article_node.csv"  \
+                            --nodes:Author="$data_path/author_node.csv"  \
+                            --nodes:Conference/Workshop="$data_path/conference_workshop_node.csv" \
+                            --nodes:Journal="$data_path/journal_node.csv"  \
+                            --nodes:Keyword="$data_path/keyword_node.csv" \
+                            --relationships:Cites="$data_path/cites_relationship.csv" \
+                            --relationships:Has_Keyword="$data_path/has_keyword_relationship.csv"  \
+                            --relationships:Presented_In="$data_path/presented_in_relationship.csv" \
+                            --relationships:Published_In="$data_path/published_in_relationship.csv"  \
+                            --relationships:Reviews="$data_path/reviews_relationship.csv" \
+                            --relationships:Writes="$data_path/writes_relationship.csv" \
